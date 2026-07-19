cea91fc7aaa357a2a8d7db96f707ff6c50a5d292
am8691472 <92670374+am8691472@users.noreply.github.com>
2026-07-19 07:48:03 -0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     experimentalSessionAndOrigin: true,
-    chromeWebSecurity: false,  // Cross-origin errors ko disable karega
+    chromeWebSecurity: false,  
   },
 });
